00c72af699308ec4c458b1445b7b8826b420e62b
Matt McElreath <matt.mcelreath@gmail.com>
2026-04-25 10:02:54 -0400

    test

--- PSMultiPass/PSMultiPass.psm1
+++ PSMultiPass/PSMultiPass.psm1
@@ -1,9 +1,9 @@
 # Implement your module commands in this script.
 #Dot source private functions.
-Get-ChildItem -Path $PSScriptRoot\Functions\Private\*.ps1 | ForEach-Object { . $_.FullName }
+Get-ChildItem -Path $PSScriptRoot\Functions\Public\*.ps1 | ForEach-Object { . $_.FullName }
 
 #Dot source public functions.
-Get-ChildItem -Path $PSScriptRoot\Functions\Public\*.ps1 | ForEach-Object { . $_.FullName }
+Get-ChildItem -Path $PSScriptRoot\Functions\Private\*.ps1 | ForEach-Object { . $_.FullName }
 
 # Export only the functions using PowerShell standard verb-noun naming.
 # Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
