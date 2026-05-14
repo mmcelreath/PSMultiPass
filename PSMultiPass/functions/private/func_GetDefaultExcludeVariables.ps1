a53e790c32b5ef7c8989de589ba833546470c5d9
Matt McElreath <matt.mcelreath@gmail.com>
2026-05-13 22:55:43 -0400

    Added comment based help (#10)
    * Added comment based help
    
    * version 0.0.7

--- PSMultiPass/functions/private/func_GetDefaultExcludeVariables.ps1
+++ PSMultiPass/functions/private/func_GetDefaultExcludeVariables.ps1
@@ -1,3 +1,27 @@
+<#
+.SYNOPSIS
+    Gets a list of default variables to exclude from parallel execution.
+
+.DESCRIPTION
+    This private function returns a collection of variable names that should be excluded
+    when importing variables into parallel script blocks. It combines:
+    - Default cmdlet binding parameters (like Verbose, Debug, etc.)
+    - Current bound parameters
+    - Standard user environment variables
+    
+.EXAMPLE
+    PS> $excludeVars = func_GetDefaultExcludeVariables
+    PS> $excludeVars | Select-Object -First 5
+    
+    Returns a list of variable names to exclude from parallel import.
+
+.NOTES
+    This is a private helper function used internally by Invoke-ForEachParallelProxy.
+    It helps prevent conflicts and unnecessary variable passing in parallel execution contexts.
+
+.LINK
+    Invoke-ForEachParallelProxy
+#>
 function func_GetDefaultExcludeVariables {
     Function _temp {
         [cmdletbinding(SupportsShouldProcess=$True)] param() 
